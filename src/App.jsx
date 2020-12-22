bf8d09f9aaa78ea99f0de6d9d82579e194407a47
DevArt002 <jr075535@gmail.com>
2020-12-23 01:51:36 +1000

    Install dependencies and configurations

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+// Import style
+import './App.css'
+
+const App = () => <h1>React App</h1>
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: require.resolve('babel-loader'),
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: require.resolve('babel-loader'),
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+}
